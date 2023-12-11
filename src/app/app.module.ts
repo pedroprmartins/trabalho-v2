cc846c32208248e0b3b1e89c7fcda6b6a1fcbe83
Paulo Regis <pauloregistecnico@gmail.com>
2023-12-10 22:06:04 -0300

    feat: Ajustes no module global do projeto.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,10 +14,11 @@ import { MatTableModule } from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDialogModule } from '@angular/material/dialog';
-import { ElementDialogComponent } from './shared/element-dialog/element-dialog.component';
+import { AlbumDialogComponent } from './shared/album-dialog/album-dialog.component';
 import { MatFormFieldModule} from "@angular/material/form-field";
 import { FormsModule } from "@angular/forms";
 import { MatInputModule } from "@angular/material/input";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -25,7 +26,7 @@ import { MatInputModule } from "@angular/material/input";
     HomeComponent,
     HeaderComponent,
     FooterComponent,
-    ElementDialogComponent,
+    AlbumDialogComponent,
   ],
   imports: [
     BrowserModule,
@@ -41,6 +42,7 @@ import { MatInputModule } from "@angular/material/input";
     MatFormFieldModule,
     FormsModule,
     MatInputModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
